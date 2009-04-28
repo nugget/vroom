3cd81b6b689e3ed9a95c81ebe7f8526530caa735
David McNett <nugget@flightaware.com>
2009-04-28 17:26:54 -0500

    Import detects and stores new vehicles now.

--- scripts/functions.tcl
+++ scripts/functions.tcl
@@ -14,3 +14,54 @@ proc sanitize_number { value } {
         }
         return "NULL"
 }
+
+proc simplesqlquery {db sql} {
+	set result [pg_exec $db $sql]
+	set status [pg_result $result -status]
+
+	if {[string match "PGRES_*_OK" $status]} {
+		set success 1
+	} else {
+		set success 0
+		puts stderr [pg_result $result -error]
+	}
+	
+	if {$success && [pg_result $result -numTuples] > 0} {
+		set return [pg_result $result -getTuple 0]
+	} else {
+		set return ""
+	}
+
+	pg_result $result -clear
+	return $return
+}
+
+proc pg_exec_or_exception {db sql} {
+	set result [pg_exec $db $sql]
+	set status [pg_result $result -status]
+
+	if {[string match "PGRES_*_OK" $status]} {
+		set success 1
+	} else {
+		set success 0
+		puts stderr [pg_result $result -error]
+	}
+
+	pg_result $result -clear
+	return $success
+}
+
+proc add_vehicle { name units_odometer units_economy notes } {
+	global dbh
+
+	set vehicle_id [simplesqlquery $dbh "SELECT vehicle_id FROM vehicles WHERE name = [pg_quote $name]"]
+	if {$vehicle_id == ""} {
+		pg_exec_or_exception $dbh "INSERT INTO vehicles (name,units_odometer,units_economy,notes) VALUES ([pg_quote $name],[pg_quote $units_odometer],[pg_quote $units_economy],[pg_quote $notes])"
+		set vehicle_id [simplesqlquery $dbh "SELECT vehicle_id FROM vehicles WHERE name = [pg_quote $name]"]
+		puts "Added new vehicle id $vehicle_id ($name) to database"
+	}
+	if {$vehicle_id != ""} {
+		return $vehicle_id
+	}
+	return -1
+}
--- scripts/import.tcl
+++ scripts/import.tcl
@@ -4,6 +4,8 @@ set ::debug 0
 
 package require Pgtcl 
 package require Tclx
+package require csv
+
 source functions.tcl
 
 proc main {} {
@@ -15,17 +17,65 @@ proc main {} {
 	set dbh [pg_connect -connlist [array get ::DB]]
 
 	set data [read_file "rt.mail"]
+
+
+	# First we need to pull out vehicle information (naturally at the bottom)
+	set live 0
+	foreach line [split $data "\n"] {
+		if {$line == "VEHICLE"} {
+			set live 1
+			set vehicle 0
+			set vbuf ""
+		}
+		if {[string match "---- End Copy and Paste ----" $line]} {
+			set live 0
+		}
+		if {$live == 1} {
+			puts $line
+			if {[string range $line 0 0] == "\""} {
+				if {$line == "\""} {
+					set vehicle 0
+					append vbuf $line
+					# set vbuf [string map {"\n" "\\n"} $vbuf]
+
+					set vals [::csv::split $vbuf]
+					add_vehicle [lindex $vals 0] [lindex $vals 1] [lindex $vals 2] [lindex $vals 3]
+
+				} else {
+					set vehicle 1
+				}
+			}
+			if {$vehicle == 1} {
+				append vbuf "$line\n"
+			}
+
+		}
+	}
+
+	# Now we can pull in our other data for the vehicles
 	set live 0
 	foreach line [split $data "\n"] {
 		if {$line == "ROAD TRIP CSV"} {
 			set live 1
+			set section VERSIONS
 		}
 		if {[string match "---- End Copy and Paste ----" $line]} {
 			set live 0
 		}
 
 		if {$live} {
-			puts $line
+			switch $section {
+				VERSIONS {
+				}
+				FILLUPS {
+				}
+				SERVICES {
+				}
+				TRIPS {
+				}
+				VEHICLES {
+				}
+			}
 		}
 	}
 }
